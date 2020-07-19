6d707e8e603ac1a85626d3a21d460e2d211a4804
Kitoo <kitoo@pop-os.localdomain>
2020-07-19 18:40:52 +0530

    model testing

---
+++ src/training/model_select.py
@@ -0,0 +1,126 @@
+import torchvision
+import torch
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from torch.backends import cudnn
+import models
+from dataset import Dataset
+
+# CUDA for PyTorch
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+cudnn.benchmark = True
+
+fcn = torch.load(models.fcn_path)
+fcn.to(device)
+
+deeplab = torch.load(models.deeplab_path)
+deeplab.to(device)
+
+def iou(outputs: torch.Tensor, labels: torch.Tensor):
+    SMOOTH = 1e-6
+    intersection = (outputs & labels).float().sum((1, 2))
+    union = (outputs | labels).float().sum((1, 2))
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+
+    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10
+
+    return thresholded.mean()
+
+img_path = "../../data/processed/CelebAMask-HQ/imgs"
+mask_path = "../../data/processed/CelebAMask-HQ/mask"
+transform = torchvision.transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], inplace=True)
+])
+
+train_data = Dataset(img_path, mask_path, transform, "train")
+trainingloader = DataLoader(valid_data, batch_size = 8)
+
+valid_data = Dataset(img_path, mask_path, transform, "val")
+validationloader = DataLoader(valid_data, batch_size = 8)
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+def test_model(model, train_loader, validation_loader, criterion, optimizer, show_epoch=True, gpu=True):
+    print("-------------------------------------------------")
+    print("MODEL  : "model.__class__.__name__)
+    accuracy_list=[]
+    loss_list=[]
+    for epoch in range(n_epochs):
+        for x, y in train_loader:
+            if gpu:
+                # Transfer to GPU
+                x, y = x.to(device), y.to(device)
+
+            model.train()
+            optimizer.zero_grad()
+            z = model(x)
+            loss = criterion(z, y)
+            loss.backward()
+            optimizer.step()
+            loss_list.append(loss.data)
+
+        if show_epoch:
+            print("Epoch : ", epoch, "    Loss : ", loss.data)
+
+        for x_test, y_test in validation_loader:
+            if gpu:
+                # Transfer to GPU
+                x_test, y_test = x_test.to(device), y_test.to(device)
+            model.eval()
+            y_test = tf(y_test)
+            z = model(x_test)
+            iou = iou(z, y_test)
+            accuracy_list.append(iou)
+    print("-------------------------------------------------")
+    return loss_list, accuracy_list
+
+
+fcn_loss, fcn_acc = test_model(fcn,
+    train_loader,
+    validation_loader,
+    criterion,
+    optimizer,
+    show_epoch=True,
+    gpu=use_cuda)
+
+deeplab_loss, deeplab_acc = test_model(deeplab,
+    train_loader,
+    validation_loader,
+    criterion,
+    optimizer,
+    show_epoch=True,
+    gpu=use_cuda)
+
+epoch = np.arrange(len(fcn_loss))
+
+plt.plot(epoch, fcn_loss, label="FCN")
+plt.plot(epoch, deeplab_loss, label="DeepLabv3")
+plt.xlabel("Training Cycles")
+plt.ylabel("Loss")
+plt.savefig('../../references/fig/loss.png')
+
+plt.plot(epoch, fcn_acc, label="FCN")
+plt.plot(epoch, deeplab_acc, label="DeepLabv3")
+plt.xlabel("Training Cycles")
+plt.ylabel("IOU Metric")
+plt.savefig('../../references/fig/iou.png')
+
+def pickle_dump(path, list):
+    with open(path, "wb") as fp:
+        pickle.dump(list, fp)
+
+pickle_dump("../../reports/performance_data/fcn_loss.txt", fcn_loss)
+pickle_dump("../../reports/performance_data/fcn_acc.txt", fcn_acc)
+pickle_dump("../../reports/performance_data/deeplab_loss.txt", deeplab_loss)
+pickle_dump("../../reports/performance_data/deeplab_acc.txt", deeplab_acc)
+
+'''
+Function to calculate iou metric, to get an accuracy of predicted mask.
+Code adapted from :
+https://www.kaggle.com/iezepov/fast-iou-scoring-metric-in-pytorch-and-numpy
+'''
--- src/training/models.py
+++ src/training/models.py
@@ -1,185 +1,50 @@
 import torch
 from torch import nn
+import torchvision
 
-def get_same_padding(kernel_size):
-    """Calculate padding size for same padding,
-    assuming stride of 1 and square kernel"""
+deeplab = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True)
+for param in deeplab.parameters():
+    param.requires_grad = False
 
-    if type(kernel_size) is tuple:
-        kernel_size = kernel_size[0]
-    pad_size = (kernel_size-1)//2
-    if kernel_size%2 == 0:
-        padding = (pad_size, pad_size+1)
-    else:
-        padding = pad_size
+fcn = torchvision.models.segmentation.fcn_resnet101(pretrained=True)
+for param in fcn.parameters():
+    param.requires_grad = False
 
-    return padding
+class Mask_deeplab(nn.Sequential):
+  def __init__(self):
+    super(Mask_deeplab, self).__init__(
+        nn.Conv2d(256, 3, kernel_size=(1, 1), stride=(1, 1))
+    )
 
+class Mask_fcn(nn.Sequential):
+  def __init__(self):
+    super(Mask_fcn, self).__init__(
+        nn.Conv2d(512, 3, kernel_size=(1, 1), stride=(1, 1))
+    )
 
-class Conv_block(nn.Sequential):
-    def __init__(self, conv_type, in_channels, out_channels, inter_channels = None):
-        if conv_type == "1x1":
-            super(Conv_block, self).__init__(
-                nn.Conv2d(
-                    in_channels,
-                    out_channels,
-                    kernel_size = 1,
-                    stride = 1,
-                    padding = get_same_padding(1)
-                ),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU6(inplace=True)
-            )
-        elif conv_type == "3x3":
-            super(Conv_block, self).__init__(
-                nn.Conv2d(
-                    in_channels,
-                    in_channels//2,
-                    kernel_size = 1,
-                    stride = 1,
-                    padding = get_same_padding(1)
-                ),
-                nn.ReLU6(inplace=True),
-                nn.Conv2d(
-                    in_channels//2,
-                    out_channels,
-                    kernel_size = 3,
-                    stride = 1,
-                    padding = get_same_padding(3)
-                ),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU6(inplace=True)
-            )
-        else:
-            super(Conv_block, self).__init__(
-                nn.Conv2d(
-                    in_channels,
-                    in_channels//2,
-                    kernel_size = 1,
-                    stride = 1,
-                    padding = get_same_padding(1)
-                ),
-                nn.ReLU6(inplace=True),
-                nn.Conv2d(
-                    in_channels//2,
-                    out_channels,
-                    kernel_size = 5,
-                    stride = 1,
-                    padding = get_same_padding(5)
-                ),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU6(inplace=True)
-            )
+class Identity(nn.Module):
+  def __init__(self):
+    super(Identity, self).__init__()
 
-class Deconv_block(nn.Sequential):
-    def __init__(self, conv_type, in_channels, out_channels):
-        if conv_type == "1x1":
-            super(Deconv_block, self).__init__(
-                nn.ConvTranspose2d(
-                    in_channels,
-                    out_channels,
-                    kernel_size = 1,
-                    stride = 1,
-                    padding = get_same_padding(1)
-                ),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU6(inplace=True)
-            )
-        elif conv_type == "3x3":
-            super(Deconv_block, self).__init__(
-                nn.ConvTranspose2d(
-                    in_channels,
-                    in_channels//2,
-                    kernel_size = 1,
-                    stride = 1,
-                    padding = get_same_padding(1)
-                ),
-                nn.ReLU6(inplace=True),
-                nn.ConvTranspose2d(
-                    in_channels//2,
-                    out_channels,
-                    kernel_size = 3,
-                    stride = 1,
-                    padding = get_same_padding(3)
-                ),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU6(inplace=True)
-            )
-        else:
-            super(Deconv_block, self).__init__(
-                nn.ConvTranspose2d(
-                    in_channels,
-                    in_channels//2,
-                    kernel_size = 1,
-                    stride = 1,
-                    padding = get_same_padding(1)
-                ),
-                nn.ReLU6(inplace=True),
-                nn.ConvTranspose2d(
-                    in_channels//2,
-                    out_channels,
-                    kernel_size = 5,
-                    stride = 1,
-                    padding = get_same_padding(5)
-                ),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU6(inplace=True)
-            )
-
-class Inception_block(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(Inception_block, self).__init__()
-
-        self.conv1x1 = Conv_block("1x1", in_channels, out_channels)
-        self.conv3x3 = Conv_block("3x3", in_channels, out_channels)
-        self.conv5x5 = Conv_block("5x5", in_channels, out_channels)
-
-    def forward(self, x):
-
-        a = self.conv1x1(x)
-        b = self.conv3x3(x)
-        c = self.conv5x5(x)
-
-        x = a + b + c
-        return x
+  def forward(self, x):
+    return x
 
 
-class Inception_block_r(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(Inception_block_r, self).__init__()
+def refit_model(model, Mask_module):
+    model.aux_classifier = Identity()
+    model.classifier[4] = Mask_module()
 
-        self.deconv1x1 = Deconv_block("1x1", in_channels, out_channels)
-        self.deconv3x3 = Deconv_block("3x3", in_channels, out_channels)
-        self.deconv5x5 = Deconv_block("5x5", in_channels, out_channels)
+    for param in model.classifier[1].parameters():
+      param.requires_grad = True
+    for param in model.classifier[2].parameters():
+      param.requires_grad = True
+    return model
 
-    def forward(self, x):
+fcn = refit_model(fcn, Mask_fcn)
+deeplab = refit_model(deeplab, Mask_deeplab)
 
-        a = self.deconv1x1(x)
-        b = self.deconv3x3(x)
-        c = self.deconv5x5(x)
+fcn_path = "../../models/fcn.pt"
+deeplab_path = "../../models/deeplab.pt"
 
-        x = a + b + c
-
-        return x
-
-class Autoencoder1(nn.Module):
-    def __init__(self):
-        super(Autoencoder1, self).__init__()
-
-        self.encode1 = Inception_block(3, 64)
-        self.conv1 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=2)
-        self.relu6 = nn.ReLU6(inplace=True)
-        self.deconv1 = nn.ConvTranspose2d(128, 64, kernel_size=(3, 3), stride=2, output_padding = 1)
-        self.decode1 = Inception_block_r(64, 3)
-
-    def forward(self, x):
-
-        x = self.encode1(x)
-        x = self.conv1(x)
-        x = self.relu6(x)
-        x = self.deconv1(x)
-        x = self.relu6(x)
-        x = self.decode1(x)
-        x = torch.sigmoid(x)
-
-        return x
+torch.save(fcn, fcn_path)
+torch.save(deeplab, deeplab_path)
