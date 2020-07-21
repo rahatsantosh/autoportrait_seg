710f4d6facd5bf79af983475fe24a42a755299d5
Rahat Santosh <29304369+rahatsantosh@users.noreply.github.com>
2020-07-21 22:55:37 +0530

    Update get_pretrained.sh

--- models/get_pretrained.sh
+++ models/get_pretrained.sh
@@ -1,1 +1,1 @@
-https://drive.google.com/drive/folders/1cF8D21yGyqogenu3DCY5fZLpAnuYvtlq?usp=sharing
+wget https://drive.google.com/u/0/uc?export=download&confirm=p8Hu&id=1j-vAaq4gngLHZXhrQT_hTyklV7JF_mhh
